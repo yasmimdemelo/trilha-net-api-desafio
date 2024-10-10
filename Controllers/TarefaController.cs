30f4f7c4659177f6c136c0352d07756e6b92da80
yasmim de melo fonseca <yasmimdemelofonseca@gmail.com>
2024-10-10 06:52:08 -0300

    feat: implementar obtenção de todas as tarefas no TarefaController

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -32,7 +32,8 @@ namespace TrilhaApiDesafio.Controllers
         public IActionResult ObterTodos()
         {
             // TODO: Buscar todas as tarefas no banco utilizando o EF
-            return Ok();
+            var tarefas = _context.Tarefas.ToList(); // Obtener todas las tareas
+            return Ok(tarefas); // Retornar la lista de tareas
         }
 
         [HttpGet("ObterPorTitulo")]
